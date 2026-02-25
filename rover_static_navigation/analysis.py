b44f2535bf14437bca343f15456098765814ef35
Sumedh.K <sumedh2975@gmail.com>
2026-02-26 01:30:53 +0530

    Add function to compute path length

--- rover_static_navigation/analysis.py
+++ rover_static_navigation/analysis.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+def compute_path_length(x, y):
+    length = 0
+    for i in range(len(x)-1):
+        dx = x[i+1] - x[i]
+        dy = y[i+1] - y[i]
+        length += np.sqrt(dx**2 + dy**2)
+    return length
